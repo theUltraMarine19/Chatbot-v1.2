2e8ddfb17c5a008cc087e7b7f7bd3dc445e650f2
euler_1998 <harshavardhanchrs@gmail.com>
2017-06-01 11:14:00 +0530

    Testing .gitignore

--- CONSTANTS/db_constants.py
+++ CONSTANTS/db_constants.py
@@ -1,5 +1,5 @@
 class myConstants:
     hostname = 'localhost'
     username = 'postgres'
-    password = 'anusha'
-    database = 'knowledge_graph'
+    password = 'Welcome@12345'
+    database = 'knowledge_graph'    
